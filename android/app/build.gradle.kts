609fd68fd8fce253c80095dd79aebe8d29cec25d
arafath306 <arafath306@gmail.com>
2026-08-07 15:49:14 +0600

    fix: Disable R8 minification to pass Play Protect check cleanly

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,14 +29,10 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-            // ProGuard enabled with custom rules to prevent Play Protect false positives.
-            // See proguard-rules.pro for details.
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
